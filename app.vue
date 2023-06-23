6cdc694d33518771b5b80a7ba3b7645756e0153e
Abdurrahman Shofy Adianto <azophy@gmail.com>
2023-06-23 10:44:36 +0700

    setup basic layout & client interaction

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+let names = [ 'Peserta 1', 'Peserta 2' ]
+</script>
+
 <template>
-  <div class="grid items-center justify-center">
-    <h1>coba coba</h1>
-  </div>
+  <main class="grid items-center justify-center min-h-screen w-screen">
+
+    <section class="text-center h-4/5 min-w-[800px]">
+      <h1 class="font-bold text-3xl">Simple Scoreboard</h1>
+      
+      <article class="flex gap-4 mt-4 h-2/3">
+        <ScorePanel :participant_name="names[0]"/>
+        <ScorePanel :participant_name="names[1]"/>
+      </article>
+    </section>
+
+  </main>
 </template>
+
---
+++ components/ScorePanel.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps(['participant_name'])
+const counter = ref(0);
+
+const counter_add = () => counter.value++;
+const counter_reduce = () => {
+  if (counter.value > 0) counter.value--;
+}
+</script>
+
+<template>
+  <div class="border border-gray-900 rounded-xl w-1/2 h-full">
+    <h2 class="w-full h-30 text-[3em] text-center font-bold">{{ participant_name }}</h2>
+    <span class="w-full h-full font-bold text-[10em] grid items-center justify-center">{{ counter }}</span>
+    <div class="flex gap-2">
+      <button type="button" class="w-1/2 h-full text-[3em] font-bold p-4 items-center bg-blue-400 hover:bg-blue-600" @click="counter_reduce">-</button>
+      <button type="button" class="w-1/2 h-full text-[3em] font-bold p-4 items-center bg-blue-400 hover:bg-blue-600" @click="counter_add">+</button>
+    </div>
+  </div>
+</template>
+
