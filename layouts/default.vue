5e5433d609fa24e18795535475510c6930e83d26
Abdurrahman Shofy Adianto <azophy@gmail.com>
2023-06-30 06:20:05 +0000

    udpate styling for navbar & footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
-  <nav class="p-4 flex items-center justify-center">
+  <nav class="p-4 flex items-center justify-center bg-neutral-200 dark:bg-neutral-700 dark:text-white">
     <NuxtLink to="/" class="font-bold text-lg hover:underline">PapanSkor</NuxtLink>
   </nav>
 
   <slot />
 
-  <footer class="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+  <footer class="p-6 text-center bg-neutral-200 dark:bg-neutral-700 dark:text-white">
     © {{ year }} Copyright: 
     <a class="cursor-pointer underline hover:no-underline" href="https://abdurrahman.adianto.id">Abdurrahman Shofy Adianto</a>.
-    <a class="cursor-pointer underline hover:no-underline" href="https://github.com/azophy/scoreboard-nuxt">Source code</a> for this website.
+    <a class="cursor-pointer underline hover:no-underline" href="https://github.com/azophy/papan-skor">Source code</a> for this website.
   </footer>
 </template>
 
